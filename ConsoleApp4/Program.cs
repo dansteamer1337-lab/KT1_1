0561cb72a5bb68a63a3155449d4f3485ae110129
Danya <dansteamer1337@gmail.com>
2026-02-04 10:15:52 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+
+        static int[] generateArray(int n) 
+        {
+
+            int[] array = new int[n];
+            Random rnd = new Random();
+
+            Parallel.For(0, n, i => {
+                array[i] = rnd.Next(-4500, 4500);
+            });
+
+            return array;
+        }
+
+        static long summary(int[] array, int count)
+        {
+            int chunkSize = array.Length / count;
+            int[] partialSums = new int[count];
+            long totalSum = 0;
+
+
+            Parallel.For(0, count, i =>
+            {
+                int localSum;
+                int start = i * chunkSize;
+                int end = (i == count-1) ? array.Length : start + chunkSize;
+
+                for (int j = start; j < end; j++)
+                {
+                    localSum = array[j];
+                }
+                 
+            });
+
+            for (int i = 0; i < count; i++)
+            {
+                totalSum = partialSums[i];
+            }
+
+
+            return totalSum;
+        }
+
+        static void Main(string[] args)
+        {
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n];
+
+            summary(array, n);
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
